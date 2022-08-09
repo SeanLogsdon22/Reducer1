7c3988ea55c908946ce761f92645817bcbf103d4
Sean Logsdon <101378568+SeanLogsdon22@users.noreply.github.com>
2022-08-09 10:50:00 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,71 @@
+import { useState, useReducer } from "react";
+
+// set up a reducer that takes in the action.type
+// splits it up into different cases
+// each case modifies on the inital state and return previous state
+// dipatching an action includes a payload which modifies the state speicifed in the reducer function
+// the inital state sets up the data structure that going to be modified
+// two type of actions: one to add one to delete
+// to add we just add the current text which we hold in a state through two way binding
+// for delete we keep a random id of each todo we add and filter out the id that matches the current one selected
+// map the state, specifically the todos object within the state, to li items on the page
+
+const Reducer = () => {
+  const [text, setText] = useState("");
+
+  const initialState = {
+    todos: []
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "DELETE":
+        return {
+          ...state,
+          todos: [...state.todos.filter((todo) => todo.id !== action.payload)]
+        };
+      case "ADD":
+        return {
+          ...state,
+          todos: [...state.todos, action.payload]
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const deleteHandler = (id) => {
+    dispatch({ type: "DELETE", payload: id });
+  };
+
+  const submitHandler = () => {
+    if (text === "") return;
+    dispatch({
+      type: "ADD",
+      payload: {
+        id: Math.random(),
+        text
+      }
+    });
+    setText("");
+  };
+
+  return (
+    <>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={submitHandler}>Submit</button>
+      <ul>
+        {state.todos.map((todo) => (
+          <div key={todo.id}>
+            <li>{todo.text}</li>
+            <button onClick={() => deleteHandler(todo.id)}>X</button>
+          </div>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Reducer;
